129141f85d7844d1057b5be8715864585ba9d870
kimddonggyu <dkkim1004@hanmail.net>
2023-01-07 21:23:11 +0900

    디자인 마무리!

--- lib/screen/meeting.dart
+++ lib/screen/meeting.dart
@@ -7,94 +7,102 @@ class Meeting extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
+        bottom: false,
         child: Container(
           color: Colors.pink[100],
           child: Column(
             children: [
               Expanded(
                 flex: 1,
-                child: Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          "U&I",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontFamily: 'parisienne',
-                            fontSize: 80.0,
-                          ),
-                        )
-                      ],
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          "우리 처음 만난날",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontFamily: 'sunflower',
-                            fontSize: 30.0,
-                          ),
-                        ),
-                      ],
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          "2023.01.07",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontFamily: 'sunflower',
-                            fontSize: 20.0,
-                          ),
-                        ),
-                      ],
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        IconButton(
-                            onPressed: () {},
-                            icon: Icon(
-                              Icons.favorite,
-                            ))
-                      ],
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          "D+1",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontFamily: 'sunflower',
-                            fontSize: 50.0,
-                            fontWeight: FontWeight.w700,
-                          ),
-                        ),
-                      ],
-                    )
-                  ],
-                ),
+                child: _Toppart(),
               ),
               Expanded(
                 flex: 1,
-                child: Column(
-                  children: [
-                    Row(
-                      children: [Text("그림")],
-                    )
-                  ],
-                ),
+                child: Image.asset('asset/img/pageView/image_1.jpeg'),
               )
             ],
           ),
         ),
       ),
+    );
+  }
+}
+
+class _Toppart extends StatelessWidget {
+  const _Toppart({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "U&I",
+              style: TextStyle(
+                color: Colors.white,
+                fontFamily: 'parisienne',
+                fontSize: 80.0,
+              ),
+            )
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Column(
+              children: [
+                Text(
+                  "우리 처음 만난날",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontFamily: 'sunflower',
+                    fontSize: 30.0,
+                  ),
+                ),
+                Text(
+                  "2023.01.07",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontFamily: 'sunflower',
+                    fontSize: 20.0,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            IconButton(
+                onPressed: () {},
+                iconSize: 60.0,
+                icon: Icon(
+                  Icons.favorite,
+                  color: Colors.red,
+                ))
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "D+1",
+              style: TextStyle(
+                color: Colors.white,
+                fontFamily: 'sunflower',
+                fontSize: 50.0,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+          ],
+        )
+      ],
     );
   }
 }
