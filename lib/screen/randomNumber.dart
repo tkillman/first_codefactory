fc2d3100a4fb4cf3062441be556b4543102ad1a8
kimddonggyu <dkkim1004@hanmail.net>
2023-01-08 11:07:54 +0900

    Map 함수로 실전적용!

--- lib/screen/randomNumber.dart
+++ lib/screen/randomNumber.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../const/color.dart';
+
 class RandomNumber extends StatefulWidget {
   const RandomNumber({super.key});
 
@@ -11,27 +13,60 @@ class _RandomNumberState extends State<RandomNumber> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: PRIMARY_COLOR,
       body: SafeArea(
-        child: Container(
+        child: Padding(
+          // symmetric 양옆, only 각각, ltbr 순서대로
+          padding: const EdgeInsets.symmetric(
+            horizontal: 16.0,
+          ),
           child: Column(
             children: [
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [Text("랜덤숫자 생성기"), Icon(Icons.settings)],
+                children: [
+                  Text(
+                    "랜덤숫자 생성기",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 30.0,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                  Icon(
+                    Icons.settings,
+                    color: RED_COLOR,
+                  )
+                ],
               ),
               Expanded(
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text("center"),
-                    Text("center"),
-                    Text("center"),
-                  ],
+                  children: [123, 456, 789]
+                      .map(
+                        (e) => Row(
+                          children: e
+                              .toString()
+                              .split('')
+                              .map(
+                                (y) => Image.asset(
+                                  'asset/img/randomNumber/$y.png',
+                                  width: 50.0,
+                                  height: 50.0,
+                                ),
+                              )
+                              .toList(),
+                        ),
+                      )
+                      .toList(),
                 ),
               ),
               SizedBox(
                 width: double.infinity,
                 child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: RED_COLOR,
+                  ),
                   onPressed: () {},
                   child: Text("button"),
                 ),
